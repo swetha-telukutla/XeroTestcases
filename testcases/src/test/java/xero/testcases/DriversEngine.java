cb2723564cf9068654f0355bd7df567c475f8252
SankarA <SankarA@Sloka-Reddy.fios-router.home>
2019-04-02 08:36:28 -0400

    Code Format

--- testcases/src/test/java/xero/testcases/DriversEngine.java
+++ testcases/src/test/java/xero/testcases/DriversEngine.java
@@ -4,21 +4,21 @@ import java.io.IOException;
 
 public class DriversEngine {
 	public static void main(String[] args) throws InterruptedException, IOException {
-		T1 tc1=new T1();
-		T1 tc2=new T1();
-		T1 tc3=new T1();
-		T1 tc4=new T1();
-		T1 tc5=new T1();
-		T1 tc6=new T1();
-		T1 tc7=new T1();
-		T1 tc8=new T1();
-		T1 tc9=new T1();
-		T1 tc10=new T1();
-		T1 tc11=new T1();
-		T1 tc12=new T1();
-		T1 tc13=new T1();
-		T1 tc15=new T1();
-		T1 tc16=new T1();
+		T1 tc1 = new T1();
+		T1 tc2 = new T1();
+		T1 tc3 = new T1();
+		T1 tc4 = new T1();
+		T1 tc5 = new T1();
+		T1 tc6 = new T1();
+		T1 tc7 = new T1();
+		T1 tc8 = new T1();
+		T1 tc9 = new T1();
+		T1 tc10 = new T1();
+		T1 tc11 = new T1();
+		T1 tc12 = new T1();
+		T1 tc13 = new T1();
+		T1 tc15 = new T1();
+		T1 tc16 = new T1();
 		tc1.t1();
 		tc2.t2();
 		tc3.t3();
@@ -26,15 +26,15 @@ public class DriversEngine {
 		tc5.t5();
 		tc6.t6();
 		tc7.t7();
-	tc8.t8();
-	tc9.t9();
+		tc8.t8();
+		tc9.t9();
 		tc10.t10();
 		tc11.t11();
 		tc12.t12();
-		//tc13.t13();
-		//tc15.t15();
+		// tc13.t13();
+		// tc15.t15();
 		tc16.t16();
-		
+
 	}
 
 }
